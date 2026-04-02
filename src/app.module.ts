e29582b004177a35fda17f6f8489732feb682150
Tran Minh Dang <tranminhdang0702@gmail.com>
2026-04-02 18:17:26 +0700

    Feature/DockerCompose_SchemaInit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, PrismaModule, AuthModule],
 })
 export class AppModule {}
